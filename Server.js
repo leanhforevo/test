7309ba7403b85adece8c78b8116d28d01f2278a6
le anh <leanhforevo@gmail.com>
2017-03-31 22:49:59 +0700

    make it better

--- Server.js
+++ Server.js
@@ -4,9 +4,9 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 server.listen(process.env.PORT || 3000);
 
-io.on("connection", (socket)=> {
+io.on("connection", function(socket) {
     console.log('Co Nguoi Ket noi:' + socket.id)
-    socket.on('message', (data) => {
+    socket.on('message', function(data) {
         console.log('Message from Client:' + data)
         io.sockets.emit('message-re', data)
     })

5a5feb3f93ec9c16820f4a2c202ab27ee011f5af
le anh <leanhforevo@gmail.com>
2017-03-31 22:10:32 +0700

    make it better

--- Server.js
+++ Server.js
@@ -2,15 +2,15 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-server.listen(process.env.PORT || 3000);
+// server.listen(process.env.PORT || 3000);
 
-io.on("connection", (socket) => {
-    console.log('Co Nguoi Ket noi:' + socket.id)
-    socket.on('message', (data) => {
-        console.log('Message from Client:'+data)
-         io.sockets.emit('message-re', data)
-    })
-});
+// io.on("connection", (socket) => {
+//     console.log('Co Nguoi Ket noi:' + socket.id)
+//     socket.on('message', (data) => {
+//         console.log('Message from Client:'+data)
+//          io.sockets.emit('message-re', data)
+//     })
+// });
 app.set('view engine','ejs');
 app.set("views","./views");
 app.listen(process.env.PORT || 8080);

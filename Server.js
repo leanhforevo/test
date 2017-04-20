4edda3f3dc159e342b144c6148fd2f34df85fb26
leanhforevo <leanhforevo@gmail.com>
2017-04-20 16:36:55 +0700

    Update Server.js

--- Server.js
+++ Server.js
@@ -4,30 +4,8 @@ const server = require("http").Server(app);
 const io = require("socket.io")(server);
 server.listen(process.env.PORT || 3000);
 
-var countClient = 0;
-var roomis = '';
-io.on("connection", function (socket) {
-    countClient++;
-    console.log('Co Nguoi Ket noi:' + socket.id)
-    socket.emit('numberRoom', { room: 1, data: 'data' })
-
-    socket.on('message', function (data) {
-        console.log('Message from Client:' , data)
-        // io.sockets.emit('messagerere', data)
-        io.sockets.in(data.room).emit('messagerere', data);
-    })
-    
-    socket.on('conn', function (data) {
-        console.log('Message from Client:', data.room)
-        // io.sockets.emit('message-re', data)
-        socket.join(data.room);
-        roomis = data.room;
-    })
-
-    socket.on('leaveRoom', function (data) {
-        console.log('Client leave Room:', data)
-        socket.leave(data);
-    })
-    
-});
+app.get('/', function (req, res) {
+   
+    res.end('You Must add page in end of url!!');
+})
 
